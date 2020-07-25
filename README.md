bf572b055c768859d357810a153fb221485a6c22
Steffan Padel <git@steffanpadel.eu>
2020-07-26 00:01:43 +0200

    update readme and setup.py

---
+++ README.md
@@ -0,0 +1,3 @@
+# py_mplus
+
+Python data bindings for manga plus, based on the javascript version found on the [website](https://mangaplus.shueisha.co.jp)
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name='mplus',
-    version='0.0.1',
-    url='https://github.com/SThomasP/py_mplus',
-    author='Steffan Padel',
-    author_email='git@steffanpadel.eu',
-    packages=setuptools.find_packages()
+    name="mplus-python",
+    version="0.0.1",
+    author="Steffan Padel",
+    author_email="git@steffanpadel.eu",
+    description="Python data bindings for mangaplus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sthomasp/py_mplus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
