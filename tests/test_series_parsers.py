a0ac7d42c3765636258c95a3411f357b4e0973bf
Steffan Padel <git@steffanpadel.eu>
2020-07-26 01:44:19 +0200

    add some tests

--- tests/test_series_parsers.py
+++ tests/test_series_parsers.py
@@ -19,6 +19,23 @@ class SeriesTests(unittest.TestCase):
         self.assertTrue(hasattr(details, 'first_chapters'))
         self.assertEqual(len(details.first_chapters), 10)
 
+    def test_hunterxhunter(self):
+        hunters = load('hunterxhunter.bin')
+        self.assertTrue(hasattr(hunters, 'success'))
+        self.assertFalse(hasattr(hunters, 'error'))
+        self.assertTrue(hasattr(hunters.success, 'title_detail_view'))
+        details = hunters.success.title_detail_view
+        self.assertTrue(hasattr(details, 'title'))
+        title = details.title
+        self.assertEqual(title.name, "Hunter x Hunter")
+        self.assertEqual(title.author, "Yoshihiro Togashi")
+        self.assertTrue(hasattr(details, 'banners'))
+        self.assertEqual(len(details.banners), 1)
+        self.assertEqual(details.banners[0].action.url,
+                         "https://www.viz.com/hunter-x-hunter?utm_source=MangaPlus&utm_campaign=HunterXHunterGN&utm_medium=referral&series=10015")
+        self.assertTrue(details.is_simul_release)
+        self.assertEqual(details.number_of_views, 26715)
+
 
 fp = pathlib.Path(__file__).parent.absolute()
 
